1dea33a176da4459c3723cfac0c7efc19c71ea64
1002058460 <90251152+1002058460@users.noreply.github.com>
2021-09-15 16:58:37 +0700

    Add files via upload

---
+++ api/biodata.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { Client } = require('pg')
+
+const main = async () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  const PORT = 5001;
+
+  const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "password",
+    database: "biodata"
+  });
+
+  client.connect();
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Hello Orenda" });
+  });
+
+  app.get("/biodata", (req, res) => {
+    client.query(`SELECT * FROM bio WHERE id=1`, (err, response) => {
+      client.end;
+      if (!err) {
+        return res.json(response.rows[0]);
+      } else {
+        return res.json({error: err.message});
+      }
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`"Server is Running on Port ${PORT}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
